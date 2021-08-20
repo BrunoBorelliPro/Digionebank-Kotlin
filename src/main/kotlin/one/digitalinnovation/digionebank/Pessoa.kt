232da49fbb27faf5c62be4839a37f0a53769ea79
Bruno Borelli <brunoborellipro@gmail.com>
2021-08-20 14:11:16 -0300

    Commit inicial

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,10 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Bruno"
     var cpf: String = "120.120.120-12"
+
+    inner class Endereco{
+        var rua: String = "Rua teste"
+    }
 }
 
 fun main(){
@@ -10,4 +14,6 @@ fun main(){
 
     println(p1.nome)
     println(p1.cpf)
+
+    println(p1.Endereco().rua)
 }
