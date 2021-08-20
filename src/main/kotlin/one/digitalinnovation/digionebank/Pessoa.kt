53f7ee8345a317f53bfa6c3dc9a9987b74399d78
Bruno Borelli <brunoborellipro@gmail.com>
2021-08-20 15:19:40 -0300

    function pessoaInfo

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -9,13 +9,14 @@ class Pessoa(nome: String, cpf: String){
     inner class Endereco{
         var rua: String = "Rua teste"
     }
+
+    fun pessoaInfo() = "Nome: $nome CPF: $cpf"
 }
 
 fun main(){
     val p1 = Pessoa("Bruno", "120.120.120-12")
 
-    println(p1.nome)
-    println(p1.cpf)
+    println(p1.pessoaInfo())
 
     println(p1.Endereco().rua)
 }
